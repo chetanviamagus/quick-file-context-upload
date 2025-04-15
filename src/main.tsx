d0aeda93ab0723407f72b5b3d46d31384dc361ce
Chetan Kumar N <chetan.nr@viamagus.com>
2025-04-15 06:24:40 +0530

    Fix GitHub Pages routing and asset loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./components/ThemeProvider";
+import { Toaster } from "./components/ui/toaster";
 
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
-
-const root = createRoot(rootElement);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/quick-file-context-upload">
+      <ThemeProvider defaultTheme="dark">
+        <App />
+        <Toaster />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
